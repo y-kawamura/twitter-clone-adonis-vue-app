8652a58859aed13bc7a704dc335c929487daa1b2
y-kawamura <tetora.y@gmail.com>
2019-10-01 19:38:51 +0900

    Setting axios

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import axios from 'axios'
+
 Vue.config.productionTip = false
+
+window.axios = axios
+axios.defaults.baseURL = 'http://127.0.0.1:3333'
 
 /* eslint-disable no-new */
 new Vue({
