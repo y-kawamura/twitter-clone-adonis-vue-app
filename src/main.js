4a8de3dca174b0ef74bc6202266af73dd0fb22da
y-kawamura <tetora.y@gmail.com>
2019-10-06 21:21:12 +0900

    Import moment js

--- src/main.js
+++ src/main.js
@@ -5,11 +5,16 @@ import App from './App'
 import router from './router'
 
 import axios from 'axios'
+import moment from 'moment'
 
 Vue.config.productionTip = false
 
 window.axios = axios
 axios.defaults.baseURL = 'http://127.0.0.1:3333'
+
+Vue.filter('timeAgo', data => moment(data).fromNow())
+Vue.filter('joined', data => moment(data).format('MMMM YYYY'))
+Vue.filter('dob', data => moment(data).format('MMMM Do YYYY'))
 
 /* eslint-disable no-new */
 new Vue({
